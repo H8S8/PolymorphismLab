f6c17fa1f8bfbc94fdb0915517a477ae904d62b4
Hannah Riley <hannah.847@btinternet.com>
2024-02-12 22:07:34 +0000

    canoe class

---
+++ PolymoprhismLab/src/main/java/Canoe.java
@@ -0,0 +1,14 @@
+public class Canoe extends Vehicle{
+
+    private int noOfOars;
+
+    public Canoe(String name, int maxNoOfOccupants, int noOfOars){
+        super(name, maxNoOfOccupants);
+        this.noOfOars = noOfOars;
+    }
+
+    @Override
+    public int averageSpeed(){
+        return 15;
+    }
+}
